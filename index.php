f545bbc521fe338059eaa98a0ef2a0d47832e6a4
estines <estines.nop@gmail.com>
2017-11-28 16:59:00 +0700

    initial

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    define ("TITLE","Drafting Page");
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title><?php echo TITLE; ?></title>
+        <!-- Meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap4 CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <!-- Customize CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div id="wrapper-header">
+            <div class="navbar" id="hd-nav">
+                <div class="nav" id="hd-navsc">
+                    <span>Social Media :</span>
+                    <a class="nav-link sc" id="fb" href=#><img src="img/glyphicons-social/png/glyphicons-social-31-facebook.png"></a>
+                    <a class="nav-link sc" id="tw" href=#><img src="img/glyphicons-social/png/glyphicons-social-32-twitter.png"></a>
+                    <a class="nav-link sc" id="ig" href=#><img src="img/glyphicons-social/png/glyphicons-social-33-instagram.png"></a>
+                    <a class="nav-link sc" id="le" href=#><img src="img/glyphicons-social/png/glyphicons-social-40-e-mail.png"></a>
+                </div>
+                <div class="nav justify-content-end" id="hd-navif">
+                    <span>Tel :</span>
+                    <a class="nav-link if" id="tb-tel" href="#">02-345-6789</a>
+                    <span>E-mail :</span>
+                    <a class="nav-link if" id="tb-mail" href="#">abc@draft.com</a>
+                </div>
+            </div>
+            <!--<div id="hd-menu"></div>
+            <div id="hd-sbox"></div>
+            <div class="row" id="hd-wrap">
+                <div class="col-md-4" id="categories"></div>
+                <div class="col-md-8" id="hd-slid"></div>
+            </div>
+        </div>
+        <div id="wrapper-content">
+            <div class="row" id="ct-wrap">
+                <div class="col-md-4" id="ct-side"></div>
+                <div class="col-md-8" id="ct-cont"></div>
+            </div>
+        </div>
+        <div id="wrapper-footer">
+            <div class="row" id="ft-wrap">
+                <div class="col-md-4" id="ft-about"></div>
+                <div class="col-md-4" id="ft-serv"></div>
+                <div class="col-md-4" id="ft-maps"></div>
+            </div> -->
+        </div>
+    </body>
+</html>
